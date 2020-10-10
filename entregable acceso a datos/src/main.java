0520c1527e5d74929820c7b2b320672b7513f220
PabloHernandezPalomo <55538583+PabloHernandezPalomo@users.noreply.github.com>
2020-10-10 13:19:26 +0200

    Version actual

--- entregable acceso a datos/src/main.java
+++ entregable acceso a datos/src/main.java
@@ -6,6 +6,8 @@ import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.EventListener;
 
 public class main {
@@ -80,6 +82,7 @@ public class main {
 
         JFileChooser fc1 = new JFileChooser();
         fc1.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
         panel1.add(panel4);
         panel4.add(fc1);
 
@@ -123,119 +126,23 @@ public class main {
         panel14.add(boton2);
 
         marco.setVisible(true);
-
+        JOptionPane optionPane = new JOptionPane();
         boton1.addActionListener( new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
 
-                JFrame marco2 = new JFrame("Practica GUI");
-                marco2.setSize(600, 400);
-                marco2.setResizable(false);
-                marco2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                marco2.setLayout(new BorderLayout());
-                marco2.setLocationRelativeTo(null);
+                String a = textField2.getText();
 
-                JPanel panel1 = new JPanel();
-                JPanel panel2 = new JPanel();
-                JPanel panel3 = new JPanel();
+                File file = new File(String.valueOf(fc1.getSelectedFile())+"\\"+a);
 
-                panel1.setLayout(new FlowLayout(FlowLayout.CENTER));
-                panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
-                panel3.setLayout(new FlowLayout(FlowLayout.CENTER));
+                if(!a.equals("")){
 
-                JLabel label1 =new JLabel("Escribir en el fichero");
-                JTextArea textArea1 = new JTextArea("Escriba aquí si deséa introducir texto en el fichero creado");
-                textArea1.setPreferredSize(new Dimension(500,250));
+                    if(file.exists()){
+                        optionPane.showMessageDialog(null, "El archivo ya existe", "Error creación", JOptionPane.WARNING_MESSAGE);
 
-                JButton boton1 = new JButton("Introducir Texto");
-                JButton boton2 = new JButton("Dejar fichero en blanco");
-
-                boton1.setPreferredSize(new Dimension(130,30));
-                boton2.setPreferredSize(new Dimension(160,30));
-
-                marco2.add(panel1,BorderLayout.NORTH);
-                panel1.add(label1);
-
-                marco2.add(panel2,BorderLayout.CENTER);
-                panel2.add(textArea1);
-
-                marco2.add(panel3,BorderLayout.SOUTH);
-                panel3.add(boton1);
-                panel3.add(boton2);
-
-                marco2.setVisible(true);
-            }
-
-        });
-
-        boton2.addActionListener( new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-
-                JFrame marco3 = new JFrame("Practica GUI");
-                marco3.setSize(350, 400);
-                marco3.setResizable(false);
-                marco3.setLocationRelativeTo(null);
-
-                marco3.setLayout(new BorderLayout());
-
-                JPanel panel1 = new JPanel();
-                JPanel panel2 = new JPanel();
-                JPanel panel3 = new JPanel();
-                JPanel panel4 = new JPanel();
-
-                panel1.setLayout(new GridLayout(6,1));
-                panel1.setBorder(new EmptyBorder(0,30,0,0));
-
-                panel3.setLayout(new FlowLayout(FlowLayout.CENTER));
-                panel4.setLayout(new FlowLayout(FlowLayout.CENTER));
-
-                JLabel label1 =new JLabel("Escoja la acción que desee realizar");
-                label1.setFont(label1.getFont().deriveFont((float) 20));
-
-                JButton boton1 = new JButton("Realizar acción");
-                boton1.setPreferredSize(new Dimension(130,30));
-
-                ButtonGroup grupobotones =new ButtonGroup();
-
-                JRadioButton radioButton1 = new JRadioButton("Mostrar contenido con la posivilidad de editarlo");
-                JRadioButton radioButton2 = new JRadioButton("Borrar archivo");
-                JRadioButton radioButton3 = new JRadioButton("Copiar archivo");
-                JRadioButton radioButton4 = new JRadioButton("Contar palabras");
-                JRadioButton radioButton5 = new JRadioButton("Contar vocales");
-                JRadioButton radioButton6 = new JRadioButton("Cifrar archivo");
-
-
-
-                grupobotones.add(radioButton1);
-                grupobotones.add(radioButton2);
-                grupobotones.add(radioButton3);
-                grupobotones.add(radioButton4);
-                grupobotones.add(radioButton5);
-                grupobotones.add(radioButton6);
-
-                marco3.add(panel3,BorderLayout.NORTH);
-                panel3.add(label1);
-                marco3.add(panel2,BorderLayout.WEST);
-                marco3.add(panel1,BorderLayout.CENTER);
-                marco3.add(panel4,BorderLayout.SOUTH);
-
-                panel4.add(boton1);
-
-                panel1.add(radioButton1);
-                panel1.add(radioButton2);
-                panel1.add(radioButton3);
-                panel1.add(radioButton4);
-                panel1.add(radioButton5);
-                panel1.add(radioButton6);
-
-                marco3.setVisible(true);
-
-                JOptionPane optionPane = new JOptionPane();
-                boton1.addActionListener( new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-
-                        if (radioButton1.isSelected()) {
+                    }else{
+                        try {
+                            file.createNewFile();
 
                             JFrame marco2 = new JFrame("Practica GUI");
                             marco2.setSize(600, 400);
@@ -273,25 +180,159 @@ public class main {
                             panel3.add(boton2);
 
                             marco2.setVisible(true);
-                        }
-                        if (radioButton2.isSelected()) {
-                            optionPane.showMessageDialog(null, null, "message", JOptionPane.PLAIN_MESSAGE);
 
-                        }
-                        if (radioButton3.isSelected()) {
-                            optionPane.showMessageDialog(null, null, "message", JOptionPane.PLAIN_MESSAGE);
-                        }
-                        if (radioButton4.isSelected()) {
-
-                        }
-                        if (radioButton5.isSelected()) {
-
-                        }
-                        if (radioButton6.isSelected()) {
-
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
                         }
                     }
-                });
+                }else{
+                    optionPane.showMessageDialog(null, "No a introducido el nombre del archivo", "Error creación", JOptionPane.WARNING_MESSAGE);
+
+                }
+
+
+            }
+
+        });
+
+        boton2.addActionListener( new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                if(fc.getSelectedFile()== null){
+                    optionPane.showMessageDialog(null, "No a seleccionado un archivo", "Archivo no encontrado", JOptionPane.WARNING_MESSAGE);
+
+                }else {
+                    File file2 = new File(String.valueOf(fc.getSelectedFile()));
+
+                    JFrame marco3 = new JFrame("Practica GUI");
+                    marco3.setSize(350, 400);
+                    marco3.setResizable(false);
+                    marco3.setLocationRelativeTo(null);
+
+                    marco3.setLayout(new BorderLayout());
+
+                    JPanel panel1 = new JPanel();
+                    JPanel panel2 = new JPanel();
+                    JPanel panel3 = new JPanel();
+                    JPanel panel4 = new JPanel();
+
+                    panel1.setLayout(new GridLayout(6, 1));
+                    panel1.setBorder(new EmptyBorder(0, 30, 0, 0));
+
+                    panel3.setLayout(new FlowLayout(FlowLayout.CENTER));
+                    panel4.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                    JLabel label1 = new JLabel("Escoja la acción que desee realizar");
+                    label1.setFont(label1.getFont().deriveFont((float) 20));
+
+                    JButton boton1 = new JButton("Realizar acción");
+                    boton1.setPreferredSize(new Dimension(130, 30));
+
+                    ButtonGroup grupobotones = new ButtonGroup();
+
+                    JRadioButton radioButton1 = new JRadioButton("Mostrar contenido con la posivilidad de editarlo");
+                    JRadioButton radioButton2 = new JRadioButton("Borrar archivo");
+                    JRadioButton radioButton3 = new JRadioButton("Copiar archivo");
+                    JRadioButton radioButton4 = new JRadioButton("Contar palabras");
+                    JRadioButton radioButton5 = new JRadioButton("Contar vocales");
+                    JRadioButton radioButton6 = new JRadioButton("Cifrar archivo");
+
+
+                    grupobotones.add(radioButton1);
+                    grupobotones.add(radioButton2);
+                    grupobotones.add(radioButton3);
+                    grupobotones.add(radioButton4);
+                    grupobotones.add(radioButton5);
+                    grupobotones.add(radioButton6);
+
+                    marco3.add(panel3, BorderLayout.NORTH);
+                    panel3.add(label1);
+                    marco3.add(panel2, BorderLayout.WEST);
+                    marco3.add(panel1, BorderLayout.CENTER);
+                    marco3.add(panel4, BorderLayout.SOUTH);
+
+                    panel4.add(boton1);
+
+                    panel1.add(radioButton1);
+                    panel1.add(radioButton2);
+                    panel1.add(radioButton3);
+                    panel1.add(radioButton4);
+                    panel1.add(radioButton5);
+                    panel1.add(radioButton6);
+
+                    marco3.setVisible(true);
+
+                    JOptionPane optionPane = new JOptionPane();
+
+                    boton1.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+
+                            if (radioButton1.isSelected()) {
+
+                                JFrame marco2 = new JFrame("Practica GUI");
+                                marco2.setSize(600, 400);
+                                marco2.setResizable(false);
+                                marco2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                                marco2.setLayout(new BorderLayout());
+                                marco2.setLocationRelativeTo(null);
+
+                                JPanel panel1 = new JPanel();
+                                JPanel panel2 = new JPanel();
+                                JPanel panel3 = new JPanel();
+
+                                panel1.setLayout(new FlowLayout(FlowLayout.CENTER));
+                                panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
+                                panel3.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+                                JLabel label1 = new JLabel("Escribir en el fichero");
+                                JTextArea textArea1 = new JTextArea("Escriba aquí si deséa introducir texto en el fichero creado");
+                                textArea1.setPreferredSize(new Dimension(500, 250));
+
+                                JButton boton1 = new JButton("Introducir Texto");
+                                JButton boton2 = new JButton("Dejar fichero en blanco");
+
+                                boton1.setPreferredSize(new Dimension(130, 30));
+                                boton2.setPreferredSize(new Dimension(160, 30));
+
+                                marco2.add(panel1, BorderLayout.NORTH);
+                                panel1.add(label1);
+
+                                marco2.add(panel2, BorderLayout.CENTER);
+                                panel2.add(textArea1);
+
+                                marco2.add(panel3, BorderLayout.SOUTH);
+                                panel3.add(boton1);
+                                panel3.add(boton2);
+
+                                marco2.setVisible(true);
+                            }
+                            if (radioButton2.isSelected()) {
+
+
+                                file2.delete();
+                                if (file2.exists()) {
+                                    optionPane.showMessageDialog(null, "El archivo no se pudo borrar", "Error al borrar el archivo", JOptionPane.PLAIN_MESSAGE);
+                                } else {
+                                    optionPane.showMessageDialog(null, "El archivo fue borrado exitosamente", "Archivo borrado", JOptionPane.PLAIN_MESSAGE);
+
+                                }
+
+                            }
+                            if (radioButton3.isSelected()) {
+                                optionPane.showMessageDialog(null, null, "message", JOptionPane.PLAIN_MESSAGE);
+                            }
+                            if (radioButton4.isSelected()) {
+
+                            }
+                            if (radioButton5.isSelected()) {
+
+                            }
+                            if (radioButton6.isSelected()) {
+
+                            }
+                        }
+                    });
+                }
             }
 
         });
